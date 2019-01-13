9f5bb8a912ad62089aa42d8f7a03a5e6ccfd346d
ahsia3 <tonyhsia22@gmail.com>
2019-01-13 13:28:34 -0500

    added files

---
+++ script.js
@@ -0,0 +1,84 @@
+
+function myFunction() {
+	var x = document.getElementById("hiddendiv");
+	x.style.display = "block";
+	x.style.opacity = "1";
+	x.style.width = "100%";
+	x.style.height = "100%";
+	x.style.position = "relative";
+}
+
+function unFunction(){
+	var x = document.getElementById("hiddendiv");
+	x.style.display = "none";
+}
+
+function edFunction(){
+	var x = document.getElementById("edu");
+	x.style.display = "block";
+}
+
+function edFunction_out(){
+	var x = document.getElementById("edu");
+	x.style.display = "none";
+}
+
+function hobbiesFunction(){
+	var x = document.getElementById("hobbies");
+	x.style.display = "block";
+}
+
+function hobbiesFunction_out(){
+	var x = document.getElementById("hobbies");
+	x.style.display = "none";
+}
+	
+function learningFunction(){
+	var x = document.getElementById("learning");
+	x.style.display = "block";
+}
+
+function learningFunction_out(){
+	var x = document.getElementById("learning");
+	x.style.display = "none";
+}
+
+function gamingFunction(){
+	var x = document.getElementById("gaming");
+	x.style.display = "block";
+}
+
+function gamingFunction_out(){
+	var x = document.getElementById("gaming");
+	x.style.display = "none";
+}
+
+function activitiesFunction(){
+	var x = document.getElementById("activities");
+	x.style.display = "block";
+}
+
+function activitiesFunction_out(){
+	var x = document.getElementById("activities");
+	x.style.display = "none";
+}
+
+function csFunction(){
+	var x = document.getElementById("cs");
+	x.style.display = "block";
+}
+
+function csFunction_out(){
+	var x = document.getElementById("cs");
+	x.style.display = "none";
+}
+
+function mathFunction(){
+	var x = document.getElementById("math");
+	x.style.display = "block";
+}
+
+function mathFunction_out(){
+	var x = document.getElementById("math");
+	x.style.display = "none";
+}
