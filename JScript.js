f30424f2289a3c8fd2d0b8a412f256adf1f6b225
ahsia3 <tonyhsia22@gmail.com>
2019-08-01 20:37:09 -0400

    update

--- JScript.js
+++
@@ -1,9 +0,0 @@
-function unhide(tag){
-	var x = document.getElementById(tag);
-	x.style.display = "block";
-}
-
-function hide(tag){
-	var x = document.getElementById(tag);
-	x.style.display = "none";
-}
